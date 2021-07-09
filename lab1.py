ab8fec4e24d0f18597b9467ad8377be4a53909ce
Priyavardhan <priyavardhan0109@gmail.com>
2021-07-09 16:57:06 +0530

    lab 1

--- lab1.py
+++ lab1.py
@@ -15,3 +15,14 @@ print('Number of negative tweets: ', len(all_negative_tweets))
 
 print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
 print('The type of a tweet entry is: ', type(all_negative_tweets[0]))
+
+fig = plt.figure(figsize=(5, 5))
+labels = 'ML-BSB-Lec', 'ML-HAP-Lec','ML-HAP-Lab'
+
+sizes = [40, 35, 25]
+
+plt.pie(sizes, labels=labels, autopct='%.2f%%',
+shadow=True, startangle=90)
+
+plt.axis('equal')
+plt.show()
